1e47eb67b1a67efa73d53a839f5a5ae970d54fb4
Tyagachev <92306548+Tyagachev@users.noreply.github.com>
2022-08-24 11:07:58 +0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Message} from './Components/Message/Message.jsx'
+
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <Message text ='Какой-то текст переданный через props'/>
+    </div>
+    
+  );
+}
+
+export default App;
