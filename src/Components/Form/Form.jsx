5158ac79383f7cd7529d90790afba1d85e745d51
Anton <570uz@mail.ru>
2022-09-04 18:08:52 +0300

    lesson-2

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
+import { AUTHOR } from "../../constans";
 
 export const Form = ({addMessage}) => {
     const [text, setText] = useState ('')
 
     const handleSubmit = (ev)=> {
         ev.preventDefault()
-        addMessage(text)
+        addMessage({
+            author: AUTHOR.user,
+            text,
+        });
         setText('')
     }
-
 
     return (
         <div>
         <form onSubmit={handleSubmit}>
-            <input type="text" value={text} onChange={(event)=>setText(event.target.value)}/>
-            <button>Отправить</button>
+            <input 
+            type="text" 
+            value={text} 
+            onChange={(event)=>setText(event.target.value)}
+            />
+            <button disabled={!text}>Отправить</button>
         </form>
-        <p>{setText}</p>
         </div>
-        
     )
 }
--- src/Components/MessageList/MessageList.jsx
+++ src/Components/MessageList/MessageList.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
-export const MessageList= ({messages}) => {
-    
+export const MessageList = ({messages}) => {
+    console.log(messages)
+    return (
+        <>
+            <ul>
+                {messages.map((el,idx)=>(<li key={idx}>{el.author}: {el.text}</li>))}
+            </ul>
+        </>
 
-
-    return(
         
-        
-        <ul>
-            {messages.map((el,idx)=>{<li key={idx}>{el}</li>})}
-        </ul>
-        
-    )
+    );
 }
